b4e3d65bfe5cb5c13c9c6bb6ec9d909cedd0270c
Johnson Yang <johnsonyang65@gmail.com>
2023-12-23 13:05:51 -0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React, { useState, useEffect } from 'react';
+import WordSearch from './WordSearch';
 
 function App() {
+  const [wordSearchGrid, setWordSearchGrid] = useState([]);
+
+  useEffect(() => {
+    // Fetch the word search grid from the server (assuming the server is running on localhost:3000)
+    fetch('http://localhost:3000/wordSearchGrid')
+      .then(response => response.json())
+      .then(data => setWordSearchGrid(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts
-        </a>
+        <WordSearch grid={wordSearchGrid} />
       </header>
     </div>
   );
 }
-
-export default App;
